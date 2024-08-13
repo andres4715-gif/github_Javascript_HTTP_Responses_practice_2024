8ddefdb3c68bff7e53801d3b8d6645e1fac6212b
andres-rios-intelex <147648981+andres-rios-intelex@users.noreply.github.com>
2024-08-13 08:59:23 -0500

    new data added

--- src/json_server_testing/posts/get_posts_data_test.js
+++ src/json_server_testing/posts/get_posts_data_test.js
@@ -1,11 +1,12 @@
 /*
 After put the server up, get data from http://localhost:3000/posts/ 
-- Print data for: 
+- Print id in a object for this user: 
         "firstName": "María",
         "lastName": "Rodríguez",
 */
 
 const getPostsData = async () => {
+        
   const data = await fetch("http://localhost:3000/posts/", {
     method: "GET",
     headers: {
@@ -18,7 +19,18 @@ const getPostsData = async () => {
 const postDataResponse = async () => {
   const gettingResponse = await getPostsData();
   const jsonResponse = await gettingResponse.json();
-  console.log(jsonResponse);
+  let variable;
+
+  for (const keys in jsonResponse) {
+    const jsonData = jsonResponse[keys];
+    if (jsonData.author.firstName === "María" && jsonData.author.lastName === "Rodríguez") {
+      variable = {
+        id: jsonData.id,
+      };
+      const finalIdToBeRemoved = JSON.stringify(variable);
+      console.log(`ID to be removed:`, finalIdToBeRemoved);
+    }
+  }
 };
 
 const main = () => {
