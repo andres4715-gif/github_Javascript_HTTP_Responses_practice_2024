0ff15d2a54a6fde371d73bb9142d279988972b47
andresRios <andres.rios@intelex.com>
2024-08-21 17:28:37 -0500

    new data

---
+++ src/json_server_testing/http_creditCart/patch_creditCart.js
@@ -0,0 +1,5 @@
+const { expect } = require("expect");
+/*
+PATCH Credit cart
+Get Name, lastName and id for any of the credit cart users witch withdrawals amount less than 300 and change plan to BASIC.
+*/
