eb0b6e2d4bf5caf41f4a9cdcfad40046a6f0a331
andresRios <andres.rios@intelex.com>
2024-08-22 07:38:47 -0500

    adding new test

--- src/json_server_testing/http_creditCart/patch_creditCart.js
+++ src/json_server_testing/http_creditCart/patch_creditCart.js
@@ -1,5 +1,56 @@
-const { expect } = require("expect");
 /*
 PATCH Credit cart
-Get Name, lastName and id for any of the credit cart users witch withdrawals amount less than 300 and change plan to BASIC.
+Get id for any of the credit cart users witch withdrawals.amount less than 400 and change plan to BASIC.
 */
+
+const request = async () => {
+  try {
+    const getCreditCart = await fetch("http://localhost:3000/creditCart/", {
+      method: "GET",
+    });
+    return getCreditCart;
+  } catch (error) {
+    console.log(`--- ❌❌❌ ${error.message}`);
+  }
+};
+
+const response = async () => {
+  try {
+    const jsonResponse = await request();
+    const jsonData = await jsonResponse.json();
+    let patchRequest;
+    for (const key in jsonData) {
+      const data = jsonData[key];
+
+      const withdrawals = data.withdrawals;
+      const amount = withdrawals.map((x) => x.amount);
+      const totalWithdrawals = amount.reduce((a, b) => a + b, 0);
+      if (totalWithdrawals < 400) {
+        const filterData = data;
+        console.log(`Id to be changed: ${filterData.id}`);
+
+        patchRequest = await fetch(
+          `http://localhost:3000/creditCart/${filterData.id}`,
+          {
+            method: "PATCH",
+            headers: {
+              Connection: "keep-alive",
+            },
+            body: `{"planType": "BASIC"}`,
+          }
+        );
+
+        const updatedData = await patchRequest.json();
+        console.log("✅✅✅ --- Final updated data: ", updatedData);
+      }
+    }
+  } catch (error) {
+    console.log(`--- ❌❌❌ ${error.message}`);
+  }
+};
+
+const main = () => {
+  return response();
+};
+
+main();
---
+++ src/json_server_testing/http_creditCart/patch_creditCart_change_user_play_by_savings.js
@@ -0,0 +1,5 @@
+/*
+PATCH Credit cart
+Get id and final data for any of the credit cart users witch withdrawals.amount less than 400 and change plan to BASIC ADVANCE.
+Get id and final data for any of the credit cart users witch withdrawals.amount greater than 400 and change plan to PREMIUM.
+*/
